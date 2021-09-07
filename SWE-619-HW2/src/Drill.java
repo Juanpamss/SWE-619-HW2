3b75431582e1a0b8e75e088859e6c5aad1cafb31
Juanpamss <juanpa_mss@hotmail.com>
2021-09-07 17:44:40 -0400

    Drill class updated

--- SWE-619-HW2/src/Drill.java
+++ SWE-619-HW2/src/Drill.java
@@ -1,32 +1,6 @@
 import java.util.Map;
 
 public class Drill {
-
-	/*public static boolean match (Map < String, String > m, String prompt, String answer) throws NullPointerException, IllegalArgumentException{
-		/*
-		 * Requires: m, prompt, answer not null; prompt is a key in Map m, ignoring case
-	   		Effects:  returns true iff the Map m maps prompt to answer, ignoring case
-		 * */
-		
-		/*if(answer != null) {
-			if(m.get(prompt.toLowerCase()) != null) {
-				if(m.get(prompt.toLowerCase()).equalsIgnoreCase(answer)) {
-					return true;
-				}
-			}else {
-				//Throws MFAE or Illegal Argument Exception
-				throw new IllegalArgumentException();
-			}
-		}else {
-			//Throws NPE
-			throw new NullPointerException();
-		}
-		
-		//If the input answer does not match
-		return false;
-
-				
-	} */
 	
 	public static boolean match (Map<String, String > m, String prompt, String answer) {
 		// Requires: m, prompt, answer not null; prompt is a key in Map m, ignoring case
